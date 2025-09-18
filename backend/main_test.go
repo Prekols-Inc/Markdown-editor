a3bd7b1b48975099280b374b3e5cab22abe11e0d
Sid0r0vich <112876276+Sid0r0vich@users.noreply.github.com>
2025-09-18 13:59:22 +0300

    Backend endpoints (#30)
    * fix: delete db dir
    
    * feat: added put method for file edit, added tests
    
    * fix: add dir storage to gitignore
    
    * fix: fix PUT /files/:filename request
    
    * fix: add test files *.md

--- backend/main_test.go
+++ backend/main_test.go
@@ -20,16 +20,19 @@ func setupTestRouter(repo repodb.FileRepository) *gin.Engine {
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
 
-	router.GET("/download/:filename", func(c *gin.Context) {
-		downloadFile(c, repo)
-	})
 	router.GET("/files", func(c *gin.Context) {
 		getAllFiles(c, repo)
 	})
-	router.POST("/upload", func(c *gin.Context) {
+	router.GET("/file/:filename", func(c *gin.Context) {
+		downloadFile(c, repo)
+	})
+	router.POST("/file/:filename", func(c *gin.Context) {
 		uploadFile(c, repo)
 	})
-	router.DELETE("/download/:filename", func(c *gin.Context) {
+	router.PUT("/file/:filename", func(c *gin.Context) {
+		editFile(c, repo)
+	})
+	router.DELETE("/file/:filename", func(c *gin.Context) {
 		deleteFile(c, repo)
 	})
 
@@ -60,7 +63,7 @@ func TestUploadFile(t *testing.T) {
 	err = writer.Close()
 	assert.NoError(t, err)
 
-	req, err := http.NewRequest("POST", "/upload", body)
+	req, err := http.NewRequest("POST", "/file/"+testFilename, body)
 	assert.NoError(t, err)
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 
@@ -81,6 +84,95 @@ func TestUploadFile(t *testing.T) {
 	assert.Equal(t, testContent, string(savedContent))
 }
 
+func TestSaveFile(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "test_save")
+	assert.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+
+	repo, err := repodb.NewLocalFileRepo(tempDir)
+	assert.NoError(t, err)
+
+	router := setupTestRouter(repo)
+
+	testContent := "Hello, World!"
+	testContentSaved := "Goodbye, World!"
+	testFilename := "test.txt"
+
+	err = repo.Create(testFilename, []byte(testContent))
+	assert.NoError(t, err)
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile("file", testFilename)
+	assert.NoError(t, err)
+	_, err = part.Write([]byte(testContentSaved))
+	assert.NoError(t, err)
+
+	err = writer.Close()
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("PUT", "/file/"+testFilename, body)
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var response map[string]interface{}
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "File saved successfully", response["message"])
+	assert.Equal(t, testFilename, response["filename"])
+
+	savedContent, err := repo.Get(testFilename)
+	assert.NoError(t, err)
+	assert.Equal(t, testContentSaved, string(savedContent))
+}
+
+func TestSaveFileNotFound(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "test_save")
+	assert.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+
+	repo, err := repodb.NewLocalFileRepo(tempDir)
+	assert.NoError(t, err)
+
+	router := setupTestRouter(repo)
+
+	testContent := "Hello, World!"
+	noExistsFile := "noexists.txt"
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile("file", noExistsFile)
+	assert.NoError(t, err)
+	_, err = part.Write([]byte(testContent))
+	assert.NoError(t, err)
+
+	err = writer.Close()
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("PUT", "/file/"+noExistsFile, body)
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+
+	var response map[string]interface{}
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+
+	assert.True(t, strings.Contains(response["error"].(string), "file not found"))
+}
+
 func TestDownloadFile(t *testing.T) {
 	tempDir, err := os.MkdirTemp("", "test_download")
 	assert.NoError(t, err)
@@ -91,12 +183,12 @@ func TestDownloadFile(t *testing.T) {
 
 	testContent := "Hello, World!"
 	testFilename := "test.txt"
-	err = repo.Save(testFilename, []byte(testContent))
+	err = repo.Create(testFilename, []byte(testContent))
 	assert.NoError(t, err)
 
 	router := setupTestRouter(repo)
 
-	req, err := http.NewRequest("GET", "/download/"+testFilename, nil)
+	req, err := http.NewRequest("GET", "/file/"+testFilename, nil)
 	assert.NoError(t, err)
 
 	w := httptest.NewRecorder()
@@ -120,7 +212,7 @@ func TestDownloadFileNotFound(t *testing.T) {
 
 	testFilename := "nonexistent.txt"
 
-	req, err := http.NewRequest("GET", "/download/"+testFilename, nil)
+	req, err := http.NewRequest("GET", "/file/"+testFilename, nil)
 	assert.NoError(t, err)
 
 	w := httptest.NewRecorder()
@@ -145,12 +237,12 @@ func TestDeleteFile(t *testing.T) {
 
 	testContent := "Hello, World!"
 	testFilename := "test.txt"
-	err = repo.Save(testFilename, []byte(testContent))
+	err = repo.Create(testFilename, []byte(testContent))
 	assert.NoError(t, err)
 
 	router := setupTestRouter(repo)
 
-	req, err := http.NewRequest("DELETE", "/download/"+testFilename, nil)
+	req, err := http.NewRequest("DELETE", "/file/"+testFilename, nil)
 	assert.NoError(t, err)
 
 	w := httptest.NewRecorder()
@@ -179,7 +271,8 @@ func TestUploadFileWithoutFile(t *testing.T) {
 
 	router := setupTestRouter(repo)
 
-	req, err := http.NewRequest("POST", "/upload", nil)
+	testFilename := "test.txt"
+	req, err := http.NewRequest("POST", "/file/"+testFilename, nil)
 	assert.NoError(t, err)
 	req.Header.Set("Content-Type", "multipart/form-data")
 
@@ -210,7 +303,7 @@ func TestGetAllFiles(t *testing.T) {
 	}
 
 	for filename, content := range testFiles {
-		err = repo.Save(filename, []byte(content))
+		err = repo.Create(filename, []byte(content))
 		assert.NoError(t, err)
 	}
 
