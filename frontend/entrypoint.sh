1f1c124271dc52c8fe381a6b806a979b960e8cb5
Dmitriy Sidorkin <dmitriysidorcool@gmail.com>
2025-09-24 02:00:27 +0300

    reset frontend

--- frontend/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sed -i "s|%%VITE_AUTH_API_BASE_URL%%|${VITE_AUTH_API_BASE_URL}|g" ./dist/index.html
-sed -i "s|%%VITE_STORAGE_API_BASE_URL%%|${VITE_STORAGE_API_BASE_URL}|g" ./dist/index.html
-
-npm run release --no-cache
