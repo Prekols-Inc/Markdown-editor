4edcca93dee1ecd70cdf884470d8921b9e98d005
Ivan Shanygin <shanygin101@yandex.ru>
2025-11-25 02:26:11 +0300

    Use react-hot-toast everywhere

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ToastProvider } from "./components/ToastProvider";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
